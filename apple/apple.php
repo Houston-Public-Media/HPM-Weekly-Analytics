0e7f3e970a1f907fb2c90b532cb91e82a04a8772
Jared Counts <jcounts@houstonpublicmedia.org>
2020-09-10 09:57:24 -0500

    Apple News Updates: - Added column offset to parser - Cleaned up warnings when columns are blank

--- apple/apple.php
+++ apple/apple.php
@@ -7,14 +7,15 @@
 		'55-64' => [],
 		'65+' => [],
 	];
-	
+
 	$sheet = 'Apple News';
 	$row = 0;
+	$ao = 0;
 	/**
 	 * Open the CSV report downloaded from Apple News. They have a way to set up weekly reports that they email you
 	 * 		which makes downloading the report a lot easier. The report referenced below is the "Channel Summary" report.
 	 * 		I then save it in the 'apple' folder with a filename 'channel-YYYY-MM-DD.csv' (same as the report end date)
-	 * 
+	 *
 	 */
 	if ( ( $handle = fopen( BASE . DS . "apple" . DS . "channel-" . $end . ".csv", "r" ) ) !== FALSE ) :
 		while ( ( $data = fgetcsv( $handle, 1000, "," ) ) !== FALSE ) :
@@ -23,63 +24,67 @@
 					// Header row for the spreadsheet
 					'Date','Total Views','Unique Views','Reach','Shares','Likes','Favorites','Saved Articles','Male Users','Female Users','Users 18-24','Users 25-34','Users 35-44','Users 45-54','Users 55-64','Users 65+'
 				];
+				if ( $data[0] == 'Channel' ) :
+					$ao = 1;
+				endif;
 			else :
+
 				/**
 				 * Mapping all of the CSV fields into the graphing data structure
 				 * Demographics by gender
 				 */
-				$graphs['apple-demo']['labels'][] = $data[0];
-				$graphs['apple-demo']['datasets'][0]['data'][] = $data[19] * 100;
-				$graphs['apple-demo']['datasets'][1]['data'][] = $data[20] * 100;
+				$graphs['apple-demo']['labels'][] = $data[0+$ao];
+				$graphs['apple-demo']['datasets'][0]['data'][] = $data[19+$ao] * 100;
+				$graphs['apple-demo']['datasets'][1]['data'][] = $data[20+$ao] * 100;
 
 				/**
 				 * Total views, unique views and overall reach
 				 */
-				$graphs['apple-reach']['labels'][] = $data[0];
-				$graphs['apple-reach']['datasets'][0]['data'][] = ( empty( intval( $data[1] ) ) ? 0 : intval( $data[1] ) );
-				$graphs['apple-reach']['datasets'][1]['data'][] = ( empty( intval( $data[2] ) ) ? 0 : intval( $data[2] ) );
-				$graphs['apple-reach']['datasets'][2]['data'][] = ( empty( intval( $data[18] ) ) ? 0 : intval( $data[18] ) );
+				$graphs['apple-reach']['labels'][] = $data[0+$ao];
+				$graphs['apple-reach']['datasets'][0]['data'][] = ( empty( intval( $data[1+$ao] ) ) ? 0 : intval( $data[1+$ao] ) );
+				$graphs['apple-reach']['datasets'][1]['data'][] = ( empty( intval( $data[2+$ao] ) ) ? 0 : intval( $data[2+$ao] ) );
+				$graphs['apple-reach']['datasets'][2]['data'][] = ( empty( intval( $data[18+$ao] ) ) ? 0 : intval( $data[18+$ao] ) );
 
 				/**
 				 * Shares, likes, favorites, saved articles
 				 */
-				$graphs['apple-engage']['labels'][] = $data[0];
-				$graphs['apple-engage']['datasets'][0]['data'][] = ( empty( intval( $data[3] ) ) ? 0 : intval( $data[3] ) );
-				$graphs['apple-engage']['datasets'][1]['data'][] = ( empty( intval( $data[9] ) ) ? 0 : intval( $data[9] ) );
-				$graphs['apple-engage']['datasets'][2]['data'][] = ( empty( intval( $data[10] ) ) ? 0 : intval( $data[10] ) );
-				$graphs['apple-engage']['datasets'][3]['data'][] = ( empty( intval( $data[13] ) ) ? 0 : intval( $data[13] ) );
+				$graphs['apple-engage']['labels'][] = $data[0+$ao];
+				$graphs['apple-engage']['datasets'][0]['data'][] = ( empty( intval( $data[3+$ao] ) ) ? 0 : intval( $data[3+$ao] ) );
+				$graphs['apple-engage']['datasets'][1]['data'][] = ( empty( intval( $data[9+$ao] ) ) ? 0 : intval( $data[9+$ao] ) );
+				$graphs['apple-engage']['datasets'][2]['data'][] = ( empty( intval( $data[10+$ao] ) ) ? 0 : intval( $data[10+$ao] ) );
+				$graphs['apple-engage']['datasets'][3]['data'][] = ( empty( intval( $data[13+$ao] ) ) ? 0 : intval( $data[13+$ao] ) );
 
 				/**
 				 * User breakdown by age range. Currently trending majority 50+
 				 * This breakdown is presented by day, so I'm saving it to the side for averaging
 				 */
-				$apple_ages['18-24'][] = $data[21] * 100;
-				$apple_ages['25-34'][] = $data[22] * 100;
-				$apple_ages['35-44'][] = $data[23] * 100;
-				$apple_ages['45-54'][] = $data[24] * 100;
-				$apple_ages['55-64'][] = $data[25] * 100;
-				$apple_ages['65+'][] = $data[26] * 100;
+				$apple_ages['18-24'][] = ( empty( $data[21+$ao] ) ? '0%' : ($data[21+$ao] * 100).'%' );
+				$apple_ages['25-34'][] = ( empty( $data[22+$ao] ) ? '0%' : ($data[22+$ao] * 100).'%' );
+				$apple_ages['35-44'][] = ( empty( $data[23+$ao] ) ? '0%' : ($data[23+$ao] * 100).'%' );
+				$apple_ages['45-54'][] = ( empty( $data[24+$ao] ) ? '0%' : ($data[24+$ao] * 100).'%' );
+				$apple_ages['55-64'][] = ( empty( $data[25+$ao] ) ? '0%' : ($data[25+$ao] * 100).'%' );
+				$apple_ages['65+'][] = ( empty( $data[26+$ao] ) ? '0%' : ($data[26+$ao] * 100).'%' );
 
 				// Save all of that info into a row in the spreadsheet
 				$sheets[$sheet][] = [
-					$data[0],
-					( empty( intval( $data[1] ) ) ? 0 : intval( $data[1] ) ),
-					( empty( intval( $data[2] ) ) ? 0 : intval( $data[2] ) ),
-					( empty( intval( $data[18] ) ) ? 0 : intval( $data[18] ) ),
-					( empty( intval( $data[3] ) ) ? 0 : intval( $data[3] ) ),
-					( empty( intval( $data[9] ) ) ? 0 : intval( $data[9] ) ),
-					( empty( intval( $data[10] ) ) ? 0 : intval( $data[10] ) ),
-					( empty( intval( $data[13] ) ) ? 0 : intval( $data[13] ) ),
-					($data[19] * 100).'%',
-					($data[20] * 100).'%',
-					($data[21] * 100).'%',
-					($data[22] * 100).'%',
-					($data[23] * 100).'%',
-					($data[24] * 100).'%',
-					($data[25] * 100).'%',
-					($data[26] * 100).'%'
+					$data[0+$ao],
+					( empty( intval( $data[1+$ao] ) ) ? 0 : intval( $data[1+$ao] ) ),
+					( empty( intval( $data[2+$ao] ) ) ? 0 : intval( $data[2+$ao] ) ),
+					( empty( intval( $data[18+$ao] ) ) ? 0 : intval( $data[18+$ao] ) ),
+					( empty( intval( $data[3+$ao] ) ) ? 0 : intval( $data[3+$ao] ) ),
+					( empty( intval( $data[9+$ao] ) ) ? 0 : intval( $data[9+$ao] ) ),
+					( empty( intval( $data[10+$ao] ) ) ? 0 : intval( $data[10+$ao] ) ),
+					( empty( intval( $data[13+$ao] ) ) ? 0 : intval( $data[13+$ao] ) ),
+					( empty( $data[19+$ao] ) ? '0%' : ($data[19+$ao] * 100).'%' ),
+					( empty( $data[20+$ao] ) ? '0%' : ($data[20+$ao] * 100).'%' ),
+					( empty( $data[21+$ao] ) ? '0%' : ($data[21+$ao] * 100).'%' ),
+					( empty( $data[22+$ao] ) ? '0%' : ($data[22+$ao] * 100).'%' ),
+					( empty( $data[23+$ao] ) ? '0%' : ($data[23+$ao] * 100).'%' ),
+					( empty( $data[24+$ao] ) ? '0%' : ($data[24+$ao] * 100).'%' ),
+					( empty( $data[25+$ao] ) ? '0%' : ($data[25+$ao] * 100).'%' ),
+					( empty( $data[26+$ao] ) ? '0%' : ($data[26+$ao] * 100).'%' )
 				];
-				$graphs['overall-totals']['apple-news']['data'] += ( empty( intval( $data[18] ) ) ? 0 : intval( $data[18] ) );
+				$graphs['overall-totals']['apple-news']['data'] += ( empty( intval( $data[18+$ao] ) ) ? 0 : intval( $data[18+$ao] ) );
 			endif;
 			$row++;
 		endwhile;
